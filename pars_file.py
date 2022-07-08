7f58b43e3a7bcffe6b777ec4e8cc8c3a8d95d062
Efim <efimpost@gmail.com>
2022-07-08 15:01:49 +0300

    добавлена обработка "не стандартных массивов"

--- pars_file.py
+++ pars_file.py
@@ -143,11 +143,53 @@ def get_time_list(web_browser, URL, wait_time=2):
         data_mass = data_from_timelist.text.split('\n')
         data_mass.pop(0)  # Убираем первый элемент "часы минуты" это лишнее
         # Сортировка данных
+        # Проверка на "не стандартные данные"
+        not_standart_data_flag = False
+        for i in range(0, len(data_mass) - 1):
+            if len(data_mass[i]) < len(data_mass[i + 1]):
+                not_standart_data_flag = True
+                break
+        if not_standart_data_flag:
+            # Обработка стандартного массива
+            for i in range(0, len(data_mass) - 1, 2):
+                temp_time_1[data_mass[i]] = tuple(data_mass[i + 1].split(' '))
+                out_data_mass[week_days[day]] = temp_time_1.copy()
+        else:
+            # Обработка сложного, не стандартного массива
+            out_data_mass[week_days[day]] = complex_mass(data_mass)
+
+
         for i in range(0, len(data_mass) - 1, 2):
             temp_time_1[data_mass[i]] = tuple(data_mass[i + 1].split(' '))
         out_data_mass[week_days[day]] = temp_time_1.copy()
     #print(out_data_mass)
     return out_data_mass
+
+def complex_mass(mass):
+    """
+    Функция обработки нестандартного массива данных
+    типа: ['12', '34', '56']
+    :param mass: нестандартный массив на входе
+    :return: обработанный массив на выходе
+    """
+    out = {}
+    hour = mass[0]
+    temp = []
+    for i in range(1, len(mass) - 1):
+        if int(mass[i]) == int(hour):
+            continue
+        elif int(mass[i]) < int(mass[i + 1]):
+            temp.append(mass[i])
+        else:
+            temp.append(mass[i])
+            out[hour] = tuple(temp.copy())
+            hour = mass[i + 1]
+            temp.clear()
+    else:
+        temp.append(mass[i + 1])
+        out[hour] = tuple(temp.copy())
+        temp.clear()
+    return out
 
 def stop_func(web_browser, base_object, cursor_object):
     """

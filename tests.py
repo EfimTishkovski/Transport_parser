05f9920f2655a0fb45ff0d0fb6173e33d99cecff
efim <schmurge22@mail.ru>
2022-07-10 22:50:02 +0300

    Добавлены файлы для тестирования, проб и ошибок)

---
+++ tests.py
@@ -0,0 +1,122 @@
+import json
+import time
+import sqlite3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+def func():
+    file = open('temp_station_tram_data.txt', 'r')
+    data = json.load(file)
+    print(data)
+    file.close()
+    return data
+
+
+def get_time_list(web_browser, URL, wait_time=2):
+    """
+    Функция получения времени отправления по остановке
+    :param web_browser: Объект браузера
+    :param URL: ссылка на страницу
+    :return: словарь с днями недели и временем отправления
+    """
+    # Дописать цикл извлечения ссылки из входного словаря
+    web_browser.get(URL)  # Подгружаем страницу
+    time.sleep(wait_time)
+    week_days = {1: 'Понедельник', 2: 'Вторник', 3: 'Среда', 4: 'Четверг',
+                 5: 'пятница', 6: 'Суббота', 7: 'Воскресенье'}
+    temp_time_1 = {}
+    out_data_mass = {}
+    for day in range(1, 8):
+        temp_time_1.clear()
+        button_monday = web_browser.find_element(By.XPATH,
+                                            f'/html/body/div[2]/div/div[2]/div[4]/div/div[3]/div[2]/div[1]/button[{day}]')
+        button_monday.click()  # Щёлкает по кнопкам дней недели
+        time.sleep(0.1)
+        data_from_timelist = web_browser.find_element(By.ID, 'schedule')
+        data_mass = data_from_timelist.text.split('\n')
+        data_mass.pop(0)  # Убираем первый элемент "часы минуты" это лишнее
+        # Сортировка данных
+
+        # Проверка на "не стандартные данные"
+        not_standart_data_flag = False
+        for i in range(0, len(data_mass) - 1):
+            if len(data_mass[i]) < len(data_mass[i + 1]):
+                not_standart_data_flag = True
+                break
+        if not_standart_data_flag:
+            # Обработка стандартного массива
+            for i in range(0, len(data_mass) - 1, 2):
+                temp_time_1[data_mass[i]] = tuple(data_mass[i + 1].split(' '))
+                out_data_mass[week_days[day]] = temp_time_1.copy()
+        else:
+            # Обработка сложного, не стандартного массива
+            print('Задействован не стандартный алгоритм')
+            out_data_mass[week_days[day]] = complex_mass(data_mass)
+
+    print(out_data_mass)
+    return out_data_mass
+
+def complex_mass(mass):
+    """
+    Функция обработки нестандартного массива данных
+    типа: ['12', '34', '56']
+    :param mass: нестандартный массив на входе
+    :return: обработанный массив на выходе
+    """
+    out = {}
+    hour = mass[0]
+    temp = []
+    for i in range(1, len(mass) - 1):
+        if int(mass[i]) == int(hour):
+            continue
+        elif int(mass[i]) < int(mass[i + 1]):
+            temp.append(mass[i])
+        else:
+            temp.append(mass[i])
+            out[hour] = tuple(temp.copy())
+            hour = mass[i + 1]
+            temp.clear()
+    else:
+        temp.append(mass[i + 1])
+        out[hour] = tuple(temp.copy())
+        temp.clear()
+    return out
+
+def write_base(mass, base):
+    """
+    Функция записи в базу
+    :param mass: Массив данных для записи
+    :param base: Объект базы
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+
+    connection = sqlite3.connect('test_base.db')
+    cursor = connection.cursor()
+
+
+    with open('temp_roads.txt', 'r') as file:
+        data = json.load(file)
+        print(data)
+        print(type(data))
+
+
+
+    cursor.close()
+    connection.close()
+
+"""
+    for i in range(5):
+        try:
+            chrome_options = webdriver.ChromeOptions()
+            #chrome_options.add_argument(argument='--headless')
+            driver = webdriver.Chrome(options=chrome_options)
+            get_time_list(driver, "https://minsktrans.by/lookout_yard/Home/Index/minsk#/routes/tram/3/stops/62208/0", wait_time=3)
+            driver.quit()
+            break
+        except:
+            print(f'страница не догружена {i}')
+"""
